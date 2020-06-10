cd9ea57504c2b898fda119f383319fe598e007f9
Derk van den Bergh <derkv@ndenbergh.nl>
2020-06-10 15:23:33 +0200

    Added lingo-words integration

---
+++ src/main/java/com/lingogame/controller/WordController.java
@@ -0,0 +1,55 @@
+package com.lingogame.controller;
+
+import com.lingogame.model.Word;
+import com.lingogame.service.IWordService;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+
+@RestController
+public class WordController {
+
+    @Autowired
+    private IWordService wordService;
+
+    @PostMapping(path = "/words/import", consumes = "application/json")
+    public String importWords(@RequestBody String params) {
+        JSONObject jsonObj = new JSONObject(params);
+        JSONArray jsonArray = (JSONArray) jsonObj.get("words");
+
+        ArrayList<Word> words = new ArrayList<>();
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            words.add(new Word(jsonArray.getString(i)));
+        }
+
+        wordService.saveAll(words);
+
+        return "succes";
+    }
+
+    @PostMapping(path = "/words/add", consumes = "application/json")
+    public String addWord(@RequestBody String params) {
+        JSONObject jsonObj = new JSONObject(params);
+
+        Word word = new Word(jsonObj.get("word").toString());
+
+        wordService.save(word);
+
+        return "succes";
+    }
+
+    @GetMapping(path = "/words", produces = "application/json")
+    public String getWords() {
+        var params = new HashMap<String, Object>();
+        params.put("words", wordService.findAll());
+
+        return new JSONObject(params).toString();
+    }
+}
--- src/main/java/com/lingogame/service/IWordService.java
+++ src/main/java/com/lingogame/service/IWordService.java
@@ -5,7 +5,12 @@ import com.lingogame.model.Word;
 import java.util.List;
 
 public interface IWordService {
+
     List<Word> findAll();
 
     Word findRandom(int length);
+
+    Iterable<Word> saveAll(Iterable<Word> words);
+
+    Word save(Word words);
 }
